a5116eb60125a1b6ff46649d11f9f23aabed9aae
jnunortiz <jnunortiz@gmail.com>
2021-06-12 21:59:39 +0200

    Implemented user methods and new errors

--- services/user_services.go
+++ services/user_services.go
@@ -6,5 +6,11 @@ import (
 )
 
 func CreateUSer(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
 	return &user, nil
 }
