214198716d14509e5d10bb31cf1f307feae3942e
jnunortiz <jnunortiz@gmail.com>
2021-06-12 17:06:02 +0200

    Implemented function to create new bad requests

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -13,11 +13,7 @@ import (
 func CreateUSer(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		restErr := errors.RestErr{
-			Message: "invalid json body",
-			Status:  http.StatusBadRequest,
-			Error:   "bad_request",
-		}
+		restErr := errors.NewBadRequestError("invalid json body")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
