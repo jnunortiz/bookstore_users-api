f0062b5cd182cd10f85129309980b94201666b40
jnunortiz <jnunortiz@gmail.com>
2021-06-14 00:59:10 +0200

    Implemented user method to find user in database given id

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/jnunortiz/bookstore_users-api/datasources/postgres/users_db"
 	"github.com/jnunortiz/bookstore_users-api/utils/date_utils"
@@ -9,26 +10,26 @@ import (
 )
 
 const (
-	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES ($1, $2, $3, $4) RETURNING id;"
-)
-
-var (
-	usersDB = make(map[int64]*User)
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES ($1, $2, $3, $4) RETURNING id;"
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = $1;"
+	UniqueEmailError = "users_email_key"
+	NoRowsError      = "no rows in result set"
 )
 
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	defer stmt.Close()
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		error := err.Error()
+		if strings.Contains(error, NoRowsError) {
+			return errors.NewNotFoundError(fmt.Sprintf("user with id %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when getting user id %d: %s", user.Id, error))
 	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 	return nil
 }
 
@@ -41,7 +42,11 @@ func (user *User) Save() *errors.RestErr {
 	user.DateCreated = date_utils.GetNowString()
 	err = stmt.QueryRow(user.FirstName, user.LastName, user.Email, user.DateCreated).Scan(&user.Id)
 	if err != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to insert user: %s", err.Error()))
+		error := err.Error()
+		if strings.Contains(error, UniqueEmailError) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to insert user. %s", error))
 	}
 	return nil
 }
